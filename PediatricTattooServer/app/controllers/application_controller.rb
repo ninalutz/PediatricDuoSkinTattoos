5890906155b5fce28245777e26c93aece984e755
Tasya Aditya Rukmana <tadityar@gmail.com>
2017-01-19 18:30:41 -0500

    Remove before_skip

--- PediatricTattooServer/app/controllers/application_controller.rb
+++ PediatricTattooServer/app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
 class ApplicationController < ActionController::Base
-	skip_before_filter  :verify_authenticity_token
-  	protect_from_forgery with: :exception
+  	protect_from_forgery unless: -> { request.format.json? }
 end
